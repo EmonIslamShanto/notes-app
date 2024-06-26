15e0c57088e4e5cb38219d7f1aa2885fce196ab0
Emon Islam Shanto <shanto35-3153@diu.edu.bd>
2024-06-26 15:50:37 +0600

    Node App

---
+++ src/components/nodes-container/index.js
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { NotesContext } from "../../App";
+import Note from "../note";
+import "./style.css";
+
+function NotesContainer(){
+
+    const {notes} = useContext(NotesContext);
+    return (
+        <div className="notes-container">
+            <h2>Notes</h2>
+            <div className="notes-list">
+                {notes.map(note => <Note note={note} key={note.id}/>)}
+            </div>
+        </div>
+    );
+}
+
+export default NotesContainer;
