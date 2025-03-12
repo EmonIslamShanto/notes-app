7a5c19cba4e7236acf2e9a4ddd20200bbae4ce3f
EmonIslamShanto <shanto35-3153@diu.edu.bd>
2025-03-12 10:32:05 +0600

    More colors added

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -7,38 +7,54 @@ const colors = [
     "#84dada",
     "#eff5b1",
     "#9adbf6",
+    "#c3aed6", // lavender
+    "#ffc1fa", // soft pink
+    "#a0e7e5", // aqua mint
+    "#b4f8c8", // mint green
+    "#fbe7c6", // peachy cream
+    "#fcd5ce", // soft coral
+    "#cdeac0", // light green
+    "#d4a5a5", // muted rose
+    "#e4bad4", // light mauve
+    "#a9def9", // soft sky blue
+    "#f6dfeb", // baby pink
+    "#ffe5b4", // light apricot
+    "#e0bbe4", // soft lilac
+    "#f4d6cc", // blush peach
+    "#d6eadf", // sage green
+
 ]
 
-function Sidebar(){
+function Sidebar() {
 
- const {addNote} = useContext(NotesContext);
+    const { addNote } = useContext(NotesContext);
 
- const [selectedTheme, setSelectedTheme] = useState(colors[0]);
+    const [selectedTheme, setSelectedTheme] = useState(colors[0]);
 
- return (
-    <div className="sidebar"> 
-        <button className="add-note-btn" onClick={() => addNote(selectedTheme)}>
-            <i className="fa fa-plus"></i>
-        </button>
-        <div className="colors-input-list">
-            {colors.map(color => (
-                <div className="color-input">
-                    <input 
-                        type="radio"
-                        name="color-input"
-                        value={color}
-                        id={"color."+color}
-                        checked = {selectedTheme === color}
-                        onChange={(e) => setSelectedTheme(color)}
-                    />
-                    <label htmlFor={"color."+color} style={{
-                        background: color
-                    }}></label>
-                </div>
-            ))}
+    return (
+        <div className="sidebar">
+            <button className="add-note-btn" onClick={() => addNote(selectedTheme)}>
+                <i className="fa fa-plus"></i>
+            </button>
+            <div className="colors-input-list">
+                {colors.map(color => (
+                    <div className="color-input">
+                        <input
+                            type="radio"
+                            name="color-input"
+                            value={color}
+                            id={"color." + color}
+                            checked={selectedTheme === color}
+                            onChange={(e) => setSelectedTheme(color)}
+                        />
+                        <label htmlFor={"color." + color} style={{
+                            background: color
+                        }}></label>
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
- );
+    );
 }
 
 export default Sidebar;
